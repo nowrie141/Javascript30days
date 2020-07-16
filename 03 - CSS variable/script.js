1870ebaefe7bd48a288af818be21be171b4e1f26
Ismael Abu-jadur García <i62abgai@uco.es>
2020-07-16 22:54:53 +0200

    Third challenge completed
    Change image properties with javascript and css

---
+++ 03 - CSS variable/script.js
@@ -0,0 +1,26 @@
+const input = document.querySelectorAll('.controls .input input');
+
+function handleValues(){
+  const suffix = this.dataset.sizing || "";
+  if (suffix === "px"){
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    console.log(this.value+suffix)
+  }else{
+    document.documentElement.style.setProperty(`--${this.name}`, this.value);
+  }
+}
+
+function changeValues(){
+  const suffix = this.dataset.sizing || "";
+  const span = document.querySelector(`.${this.name}`);
+  if (suffix === "px"){
+    span.innerText = this.value + suffix;
+  }else{
+    span.innerText = this.value;
+  }
+
+}
+
+input.forEach(input => input.addEventListener('change', handleValues));
+input.forEach(input => input.addEventListener('change', changeValues));
+input.forEach(input => input.addEventListener('mousemove', changeValues));
